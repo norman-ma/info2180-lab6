46e638fdc4a81cd0fb746d971c647366afe564c8
Michael Anthony Norman <michaelanthony.norman@mymona.uwi.edu>
2016-11-08 05:01:55 +0000

    exercise four

--- request.php
+++ request.php
@@ -1,18 +1,52 @@
 <?php
-
 // accept a term (keyword)
 // respond with a value
-
 $query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
+$all= $_GET['all'];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+    
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition name="definition" author="Tiffany">
+    A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    
+    <definition name="bar" author="Michael">
+    A place that sells alcholic beverages
+    </definition>
+    
+    <definition name="ajax" author="LeJeanna">
+    Technique which involves the use of javascript and xml (or JSON)
+    </definition>
+    
+    <definition name="html" author="Jon">
+    The standard markup language for creating web pages and web applications.
+    </definition>
+    
+    <definition name="css" author="Anthony">
+    A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    
+    <definition name="javascript" author="Norman">
+    A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    
+    <definition name="php" author="Fife">
+    A server-side scripting language, and a powerful tool for making dynamic and interactive websites
+    </definition>
+</entries>';
+
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+
+if($all=='true'){
+    print $xmlOutput->asXML();
+}else{
+    foreach($xmlOutput->definition as $def){
+        if((string)$def['name'] == $query){
+            $x = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><entries>'.$def->asXML().'</entries>');
+            print $x->asXML();
+            break;
+        }
+    }
+}
